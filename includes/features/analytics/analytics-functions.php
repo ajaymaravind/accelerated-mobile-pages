81fdc216f1cd85f7eea8d400793f50759ff56716
Ajay Aravind <ajay@midnay.com>
2021-01-14 11:00:07 +0530

    Edited lines 48 and 54 in regard with issue #4905
    Inorder to clear the issue #4905, changed AMP analytics script type in lines 48 and 54. Please test and pull.

--- includes/features/analytics/analytics-functions.php
+++ includes/features/analytics/analytics-functions.php
@@ -45,13 +45,13 @@ function ampforwp_analytics() {
 			$ampforwp_ga_fields = preg_replace('/\n\s*\n/', '', $ampforwp_ga_fields);
 	 		?>
 	 		<amp-analytics <?php if(ampforwp_get_data_consent()){?>data-block-on-consent <?php } ?> type="gtag" id="analytics1">
-	 		<script type="application/json">
+	 		<script type="application/ld+json">
 				<?php echo $ampforwp_ga_fields; ?>
 			</script>
 			</amp-analytics>
 	 		<?php } else { ?>
 			<amp-analytics <?php if(ampforwp_get_data_consent()){?>data-block-on-consent <?php } ?> type="gtag" id="analytics1" data-credentials="include" >
-				<script type="application/json">
+				<script type="application/ld+json">
 					<?php echo $ampforwp_ga_fields; ?>
 				</script>
 			</amp-analytics>
